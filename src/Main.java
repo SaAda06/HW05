6c2dcb018348764209227caa393be4f44d54208e
SaAda06 <saadi9406@icloud.com>
2022-11-25 18:12:48 +0200

    HW06

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,27 @@
+import PublicTransport.Transport;
+
 public class Main {
     public static void main(String[] args) {
-        Dog firstDog = new Dog("Dog", 0);
-        Dog secondDog = new Dog("New Dog", 15);
-        Dog thirdDog = new Dog("Third Dog", 12);
+        Transport transport = new Transport();
+        transport.setName("Volvo");
+//        transport.getName();
+        System.out.println(transport);
+        transport.setSpotsCount(4);
+        transport.setSpotsCount(10);
+        transport.move(2);
+        transport.move(20);
 
-        Client firstClient = new Client("Ivan", 10000, 364525452);
-        Client secondClient = new Client("Boris",20000, 452585612);
-        Client thirdClient = new Client("John",100, 415853128);
 
     }
 }
+//    public static void main(String[] args) {
+//        Dog firstDog = new Dog("Dog", 0);
+//        Dog secondDog = new Dog("New Dog", 15);
+//        Dog thirdDog = new Dog("Third Dog", 12);
+//        Client firstClient = new Client("Ivan", 10000, 364525452);
+//        Client secondClient = new Client("Boris",20000, 452585612);
+//        Client thirdClient = new Client("John",100, 415853128);
+//
+//  }
+
+
